145f62619c358ba3d0b89da15c92a747acb90ee6
Annette Odingo <odingoannette@gmail.com>
2019-12-18 13:13:02 +0000

    Create a form to update movie details

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
+
 function UpdateMovie(){
     return (
-        <div>Hello</div>
+        <div className='updS'>
+            <form>
+                <label> Title:
+                    <span>
+                        <input type='text' />
+                    </span>
+                </label>
+                <br />
+                <label> Director:
+                    <span>
+                        <input type='text' />
+                    </span>
+                </label>
+                <br />
+                <label> Metascore:
+                    <span>
+                        <input type='number' />
+                    </span>
+                </label>
+                <br />
+                <label>Star1:
+                    <span> 
+                        <input type='text' />
+                    </span>
+                </label>
+                <br />
+                <label> Star2:
+                    <span>
+                        <input type='text' />
+                    </span>
+                </label>
+                <br />
+                <label>Star3:
+                    <span> 
+                        <input type='text' />
+                    </span>
+                </label>
+                <br />
+                <button>Update Movie</button>
+            </form>
+        </div>
     )
 }
 export default UpdateMovie;
